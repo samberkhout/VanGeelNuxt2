150fbfbcaaf8c4c04f2ff0e46b5ede858887411f
samberkhout <144678304+samberkhout@users.noreply.github.com>
2025-06-05 17:29:33 +0200

    Add search to RasSelect component

--- components/RasSelect.vue
+++ components/RasSelect.vue
@@ -1,10 +1,22 @@
 <template>
-  <select :value="modelValue" @change="update($event.target.value)">
-    <option disabled value="">Kies ras</option>
-    <option v-for="soort in soorten" :key="soort.id" :value="soort.id">
-      {{ soort.naam }}
-    </option>
-  </select>
+  <div class="ras-select">
+    <input
+      v-model="search"
+      class="search-input"
+      type="text"
+      placeholder="Zoek ras"
+    />
+    <select :value="modelValue" @change="update($event.target.value)">
+      <option disabled value="">Kies ras</option>
+      <option
+        v-for="soort in filteredSoorten"
+        :key="soort.id"
+        :value="soort.id"
+      >
+        {{ soort.naam }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -13,7 +25,28 @@ const emit = defineEmits(['update:modelValue']);
 
 const { data: soorten } = await useFetch('/api/soorten');
 
+const search = ref('');
+const filteredSoorten = computed(() =>
+  (soorten.value || []).filter((s: any) =>
+    s.naam.toLowerCase().includes(search.value.toLowerCase())
+  )
+);
+
 function update(value: string) {
   emit('update:modelValue', Number(value));
 }
 </script>
+
+<style scoped>
+.ras-select {
+  display: flex;
+  flex-direction: column;
+}
+
+.search-input {
+  margin-bottom: 0.5rem;
+  padding: 0.25rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
