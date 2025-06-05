b50e1507220d18ce917306f36ee200c0a89df123
samberkhout <144678304+samberkhout@users.noreply.github.com>
2025-06-05 16:54:33 +0200

    feat: manage leveranciers en soorten

---
+++ server/api/soorten/[id].ts
@@ -0,0 +1,20 @@
+import { prisma } from '../prisma'
+import { z } from 'zod'
+
+const schema = z.object({
+  naam: z.string().optional(),
+  leverancierId: z.number().optional()
+})
+
+export default defineEventHandler(async (event) => {
+  const id = Number(event.context.params?.id)
+  if (event.req.method === 'PATCH') {
+    const body = await readBody(event)
+    const data = schema.parse(body)
+    return await prisma.soort.update({ where: { id }, data })
+  }
+  if (event.req.method === 'DELETE') {
+    await prisma.soort.delete({ where: { id } })
+    return { success: true }
+  }
+})
