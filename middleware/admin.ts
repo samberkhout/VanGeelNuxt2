7e9f315b2a86893bc51c71ba9f7afbb923f2a600
samberkhout <s1199368@student.windesheim.nl>
2025-06-25 16:29:03 +0200

    q

--- middleware/admin.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(async () => {
-  const { data } = await useFetch('/api/auth/me', {
-    headers: useRequestHeaders(['cookie'])
-  })
-  if (!data.value || data.value.rol !== 'ADMIN') {
-    return navigateTo('/login')
-  }
-})
--- server/api/soorten.get.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from '../prisma'
-
-export default defineEventHandler(async () => {
-  return await prisma.soort.findMany({
-    select: { id: true, naam: true, leverancierId: true, leverancier: { select: { naam: true } } }
-  })
-})
