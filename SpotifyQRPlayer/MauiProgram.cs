898128d0d590d255783a5114ba16f85b9c29fcca
samberkhout <144678304+samberkhout@users.noreply.github.com>
2025-06-27 21:35:13 +0200

    Replace BarcodeScanner with camera page

--- SpotifyQRPlayer/MauiProgram.cs
+++ SpotifyQRPlayer/MauiProgram.cs
@@ -1,6 +1,7 @@
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using ZXing.Net.Maui.Controls;
+using ZXing.Net.Maui;
 
 namespace SpotifyQRPlayer;
 
@@ -11,11 +12,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .ConfigureMauiHandlers(h =>
-            {
-                h.AddHandler(typeof(CameraBarcodeReaderView), typeof(CameraBarcodeReaderViewHandler));
-                h.AddHandler(typeof(BarcodeGeneratorView), typeof(BarcodeGeneratorViewHandler));
-            });
+            .UseBarcodeReader();
         return builder.Build();
     }
 }
